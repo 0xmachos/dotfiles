284771fe5480bf99906850ef48756a4eab958f74
0xmachos <0xmachos@gmail.com>
2017-10-06 13:46:51 +0100

    Inital commit
    Initial functions defaults and change_vmware_home
    
    defaults:
    - Make TextEdit open new documents as plain text rather than rich text
    - Change Mail Drop trigger size to 5MB instead of 20MB
    - Show the battery percentage in the menu bar
    
    change_vmware_home:
    - Change VMWare VM default location to home folder instead of Documents as Documents now lives in iCloud

---
+++ bin/bittersweet.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -euo pipefail
+
+# funfair.sh
+#	Configures macOS just the way I like it
+# Tested on:
+#	10.13
+
+function usage {
+	echo -e "\nConfigures macOS just the way I like it 🤓\n"
+	echo "Usage:"
+	echo "	defaults	- Write new system and application default settings"
+	echo "	vmware 		- Change VMWare defaultVMPath to $HOME/Virtual Machines" 
+	
+
+	echo -e "\n 	\033[0;31mhailmary\033[0m 	- Run every function in order listed above"
+
+	exit 0
+}
+
+
+function defaults {
+
+	echo "[🍺] Writing some system & application defaults"
+
+    defaults write com.apple.TextEdit RichText -int 0
+    # TextEdit: Use Plain Text Mode as Default
+    # Default: com.apple.TextEdit RichText -int 1
+
+	defaults write com.apple.mail minSizeKB 5000
+	# Mail: If attatchment is over 5MB ask to send via Mail Drop
+	# Default: com.apple.mail minSizeKB 20000
+
+	defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+	# Menu Bar: Show battery percentage
+	# Default: com.apple.menuextra.battery ShowPercent -string "NO"
+
+	exit 0
+}
+
+
+function change_vmware_home {
+
+	echo "[🍺] Setting VMWare VM Default Location"
+
+	if mkdir $HOME/Virtual\ Machines ; then
+		echo "[✅] Successfully created $HOME/Virtual Machines"
+		
+		if echo 'prefvmx.defaultVMPath = "'$HOME'/Virtual Machines/"' >> ~/Library/Preferences/VMWare\ Fusion/preferences ; then
+			echo "[✅] Successfully changed prefvmx.defaultVMPath"
+			exit 0
+		else
+			echo "[❌]Failed to change prefvmx.defaultVMPath to $HOME/Virtual Machines"
+			exit 1
+		fi
+
+	else
+		echo "[❌]Failed to create $HOME/Virtual Machines"
+		exit 1
+	fi
+}
+
+
+function main {
+	local cmd=${1:-"usage"}
+
+    if [[ $cmd == "defaults" ]]; then
+        defaults
+
+    elif [[ $cmd == "vmware" ]]; then
+		change_vmware_home
+
+    elif [[ $cmd == "hailmary" ]]; then
+    # Execute all the functions
+		# Order matters!
+		# TODO: Manually adding new fucntions sucks
+		echo -e "[🍺] \033[0;31mHailmary\033[0m engaged "
+
+		#defaults
+		#change_vmware_home
+
+	else
+		usage
+	fi
+}
+
+main "$@"
